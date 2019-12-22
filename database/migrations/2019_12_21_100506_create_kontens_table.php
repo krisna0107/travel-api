378037f5a3c3797e9c9f5b72c07b70409d6f41f6
krisna0107 <krisnayanajavista@gmail.com>
2019-12-22 09:27:54 +0700

    up

---
+++ database/migrations/2019_12_21_100506_create_kontens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKontensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kontens', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('judul');
+            $table->bigInteger('harga');
+            $table->text('url_photo');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kontens');
+    }
+}
