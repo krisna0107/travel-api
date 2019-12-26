da67c9820c93aaa6a02c2c1ecf8de7c90d050f12
krisna0107 <krisnayanajavista@gmail.com>
2019-12-26 14:17:14 +0700

    cart

---
+++ database/migrations/2019_12_26_060849_create_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->string('kd_book');
+            $table->bigInteger('konten_id');
+            $table->bigInteger('user_id');
+            $table->bigInteger('harga');
+            $table->enum('status', ['P', 'D']);
+            $table->primary('kd_book');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
