bc84cffad0ba3443581258cec500b7da5392704a
krisna0107 <krisnayanajavista@gmail.com>
2019-12-31 20:07:07 +0700

    set seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(AkunSeeder::class);
+        $this->call(KontenSeeder::class);
     }
 }
