44748d4318a4c18efe9f1f6d5ebd429a647795f0
krisna0107 <krisnayanajavista@gmail.com>
2020-01-05 16:04:30 +0700

    fix add akun

--- app/Http/Controllers/AkunController.php
+++ app/Http/Controllers/AkunController.php
@@ -9,11 +9,11 @@ class AkunController extends Controller
     public function getAkun($email){
         $akun = Akun::where('email', $email)->first();
         if(!$akun){
-            return $akun;
-        }else{
             $akun->email = $email;
             $akun->save();
-
+            
+            return $akun;
+        }else{
             return $akun;
         }
     }
