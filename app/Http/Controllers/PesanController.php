e72190b757a6ad6913c13c59352658fe6acf96c9
krisna0107 <krisnayanajavista@gmail.com>
2020-01-08 21:22:44 +0700

    back

--- app/Http/Controllers/PesanController.php
+++ app/Http/Controllers/PesanController.php
@@ -27,7 +27,7 @@ class PesanController extends Controller
         if($getdata!=null)
             return ++$getdata->kd_book;
         else
-            return $datenow."-00001";
+            return "B-".$datenow."-00001";
     }
 
     public function tambah(request $request, $userid){
