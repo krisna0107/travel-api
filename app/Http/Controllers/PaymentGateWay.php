f32aa11a891aa23c993706ee8c42992839c3694f
krisna0107 <krisnayanajavista@gmail.com>
2020-01-08 22:36:16 +0700

    not header ixudra

--- app/Http/Controllers/PaymentGateWay.php
+++ app/Http/Controllers/PaymentGateWay.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Midtrans\Config;
 use Midtrans\Snap;
+use Ixudra\Curl\Facades\Curl;
 
 class PaymentGateWay extends Controller
 {
@@ -23,7 +24,7 @@ class PaymentGateWay extends Controller
     }
 
     public function getVa($amount, $kdbook, $userid, $bank){
-        $curl = curl_init();
+        // $curl = curl_init();
 
         $data = array(
             "payment_type" => "bank_transfer",
@@ -36,27 +37,39 @@ class PaymentGateWay extends Controller
                 "va_number" => $userid
                 )
         );
-        $payload = json_encode($data);
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.sandbox.midtrans.com/v2/charge",
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_HTTPHEADER => array(
-                // Set Here Your Requesred Headers
-                'Accept: application/json',
+        $header = array('Accept: application/json',
                 'Authorization: Basic U0ItTWlkLXNlcnZlci1sV3JtYzU5ZjkxNEFxOFY1X0gwVUlIcU46',
-                'Content-Type: application/json',
-            ),
-            CURLOPT_POSTFIELDS => $payload,
-        ));
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
+                'Content-Type: application/json');
+        $response = Curl::to('https://api.sandbox.midtrans.com/v2/charge')
+        ->withHeader('Accept: application/json')
+        ->withHeader('Authorization: Basic U0ItTWlkLXNlcnZlci1sV3JtYzU5ZjkxNEFxOFY1X0gwVUlIcU46')
+        ->withHeader('Content-Type: application/json')
+        ->withData( $data )
+        ->asJson()
+        ->post();
+        // return $response;
+        return response()->json($response);
+        // $payload = json_encode($data);
 
-        if ($err) {
-            return "cURL Error #:" . $err;
-        } else {
-            return response()->json(json_encode($response));
-        }
+        // curl_setopt_array($curl, array(
+        //     CURLOPT_URL => "https://api.sandbox.midtrans.com/v2/charge",
+        //     CURLOPT_CUSTOMREQUEST => "POST",
+        //     CURLOPT_HTTPHEADER => array(
+        //         // Set Here Your Requesred Headers
+        //         'Accept: application/json',
+        //         'Authorization: Basic U0ItTWlkLXNlcnZlci1sV3JtYzU5ZjkxNEFxOFY1X0gwVUlIcU46',
+        //         'Content-Type: application/json',
+        //     ),
+        //     CURLOPT_POSTFIELDS => $payload,
+        // ));
+        // $response = curl_exec($curl);
+        // $err = curl_error($curl);
+        // curl_close($curl);
+
+        // if ($err) {
+        //     return "cURL Error #:" . $err;
+        // } else {
+        //     return response()->json(json_encode($response));
+        // }
     }
 }
